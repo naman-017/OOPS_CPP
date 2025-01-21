0dc832ecd121aaec5ebf7fccd69f9c5d659ef3b8
naman-017 <92266293+naman-017@users.noreply.github.com>
2025-01-21 13:52:28 -0800

    Update OOPS1.cpp

--- OOPS1.cpp
+++ OOPS1.cpp
@@ -27,7 +27,7 @@ using namespace std;
 // 	}
 // };
 
-
+//-----------------------------------------------------------------------------------------------------------------------------------
 
 // 	ENCAPSULATION
 // class Employee{
@@ -58,7 +58,7 @@ using namespace std;
 
 
 
-
+//--------------------------------------------------------------------------------------------------------------------------------------
 
 //  ABSTRACTION
 // class AbstractClass{
@@ -117,6 +117,192 @@ using namespace std;
 // 	emp1.IntroduceYourself();
 // 	emp1.askForPromotion();
 // 	emp2.askForPromotion();
+
+// 	return 0;
+// }
+
+
+//--------------------------------------------------------------------------------------------------------------------------------------
+
+// INHERITANCE
+// class AbstractClass{
+// 	virtual void askForPromotion()=0;
+// };
+// class Employee: AbstractClass{
+// private:
+// 	string Name;
+// 	string Company;
+// 	int Age;
+// public:
+// 	void setName(string name){
+// 		Name=name;
+// 	}
+// 	string getName(){
+// 		return Name;
+// 	}
+// 	void setCompany(string company){
+// 		Company=company;
+// 	}
+// 	string getCompany(){
+// 		return Company;
+// 	}
+// 	void setAge(int age){
+// 		Age=age;
+// 	}
+// 	int getAge(){
+// 		return Age;
+// 	}
+
+// 	void IntroduceYourself(){
+// 		cout << "Name : " << Name << endl;
+// 		cout << "Company : " << Company << endl;
+// 		cout << "Age : " << Age << endl;
+// 	}
+
+// 	Employee(string name, string company, int age){
+// 		Name=name;
+// 		Company=company;
+// 		Age=age;
+// 	}
+
+// 	void askForPromotion(){
+// 		if(Age>35){
+// 			cout << Name << " is promoted!" << endl;
+// 		}else{
+// 			cout << "Sorry, no promotion for " << Name << endl;
+// 		}
+// 	}
+// };
+
+// class Developer: public Employee{
+// public:
+// 	string FavProgrammingLanguage;
+// 	Developer(string name, string company, int age, string favProgrammingLanguage)
+// 		:Employee(name, company, age){
+// 			FavProgrammingLanguage=favProgrammingLanguage;
+// 	}
+// 	void FixBugs(){
+// 		cout << getName() << " fix bugs in " << FavProgrammingLanguage << endl;
+// 	}
+// };
+
+// class Teacher: public Employee{
+// public:
+// 	string Subject;
+// 	void PrepareLesson(){
+// 		cout << getName() << " is preparing " << Subject << " lesson" << endl;
+// 	}
+// 	Teacher(string name, string company, int age, string subject)
+// 		:Employee(name, company, age){
+// 			Subject=subject;
+// 	}
+// };
+
+// int main(){
+// 	Developer emp1=Developer("Naman", "Google", 20, "C++");
+// 	Teacher t=Teacher("Saldina", "CodeBeauty", 36, "OOPS");
+// 	t.PrepareLesson();
+// 	t.askForPromotion();
+
+// 	return 0;
+// }
+
+
+
+//--------------------------------------------------------------------------------------------------------------------------------------
+
+
+// POLYMORPHISM
+// class AbstractClass{
+// 	virtual void askForPromotion()=0;
+// };
+// class Employee: AbstractClass{
+// private:
+// 	string Name;
+// 	string Company;
+// 	int Age;
+// public:
+// 	void setName(string name){
+// 		Name=name;
+// 	}
+// 	string getName(){
+// 		return Name;
+// 	}
+// 	void setCompany(string company){
+// 		Company=company;
+// 	}
+// 	string getCompany(){
+// 		return Company;
+// 	}
+// 	void setAge(int age){
+// 		Age=age;
+// 	}
+// 	int getAge(){
+// 		return Age;
+// 	}
+
+// 	void IntroduceYourself(){
+// 		cout << "Name : " << Name << endl;
+// 		cout << "Company : " << Company << endl;
+// 		cout << "Age : " << Age << endl;
+// 	}
+
+// 	Employee(string name, string company, int age){
+// 		Name=name;
+// 		Company=company;
+// 		Age=age;
+// 	}
+
+// 	void askForPromotion(){
+// 		if(Age>35){
+// 			cout << Name << " is promoted!" << endl;
+// 		}else{
+// 			cout << "Sorry, no promotion for " << Name << endl;
+// 		}
+// 	}
+// 	void Work(){
+// 		cout << Name << " is checking emails, backlogs tasks " << endl;
+// 	}
+// };
+
+// class Developer: public Employee{
+// public:
+// 	string FavProgrammingLanguage;
+// 	Developer(string name, string company, int age, string favProgrammingLanguage)
+// 		:Employee(name, company, age){
+// 			FavProgrammingLanguage=favProgrammingLanguage;
+// 	}
+// 	void FixBugs(){
+// 		cout << getName() << " fix bugs in " << FavProgrammingLanguage << endl;
+// 	}
+// 	void Work(){
+// 		cout << getName() << " is coding in " << FavProgrammingLanguage << endl;
+// 	}
+// };
+
+// class Teacher: public Employee{
+// public:
+// 	string Subject;
+// 	void PrepareLesson(){
+// 		cout << getName() << " is preparing " << Subject << " lesson" << endl;
+// 	}
+// 	Teacher(string name, string company, int age, string subject)
+// 		:Employee(name, company, age){
+// 			Subject=subject;
+// 	}
+// 	void Work(){
+// 		cout << getName() << " is Teacher " << Subject << endl;
+// 	}
+// };
+
+// int main(){
+// 	Developer d=Developer("Naman", "Google", 20, "C++");
+// 	Teacher t=Teacher("Saldina", "CodeBeauty", 36, "OOPS");
+// 	Employee e=Employee("Jack", "Narula Tech", 40);
+// 	t.Work();
+// 	d.Work();
+// 	e.Work();
+
 
 // 	return 0;
 // }
